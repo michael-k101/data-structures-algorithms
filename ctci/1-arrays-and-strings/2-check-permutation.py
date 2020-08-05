05b613ceac2311c2d06430f330ae9e5c71f61b84
Michael Koohang <michael.s.k1001@gmail.com>
2020-08-05 00:44:49 -0400

    check-permutation

--- ctci/1-arrays-and-strings/2-check-permutation.py
+++ ctci/1-arrays-and-strings/2-check-permutation.py
@@ -1,52 +1,38 @@
 # Check Permutation: Given two strings, write a method to decide if one is a permutation of the
 # other.
 # Hints: #1, #84, #122, #131
-# Example: 
-# 1. "ba", "ab" --> TRUE
-# 2. "logan", "ol" --> FALSE
-# 3. "Logan", "olgan" --> FALSE
-
-# Input: 2 strings
-# Output: bool
-# ---------------------------------------------------------------------------------------
-# Questions
-# 1. What examples can I get? "dog!!l" != "d      og"
-# 2. Can two permutations of different sizes be TRUE? No
-# 3. Is it case sensitive? Yes
-# 4. Do we count whitespace? Yes
-
-# Psuedocode Algorithm
-# ---------------------------------------------------------------------------------------
-# Check if strings are equal sizes/lengths. If not, return false, else continue
-# add every value from first string to an arrayList
-# check character by character of second string is in arrayList
-# if so, remove that element from arrayList
-# if not, return false
-# if youve iterated through entire length of second string, return true
 
 import unittest
 
-def unique(v1, v2):  
-  seen_hash = set()
-  for char in value:
-    if char == ' ':
-      continue
-    if char in seen_hash:
-      continue
-    
-  
+def is_permutation(s1, s2):  
+  char_count = {}
+  for char in s1:
+    try:
+      char_count[char] += 1
+    except KeyError:
+      char_count[char] = 1
+  for char in s2:
+    try:
+      char_count[char] -= 1
+    except KeyError:
+      return False
+  for val in char_count.values():
+    if val != 0:
+      return False
+  return True    
 
 class Test(unittest.TestCase):
 
   def test1(self):
-    self.assertEqual(unique("1234567890qwertyuiopasdfghjkl;zxcvbnm,./'[]\=-!@#$%^&*()"), True)
+    self.assertEqual(is_permutation("aab","baa"), True)
 
   def test2(self):
-    self.assertEqual(unique("  "), True)
+    self.assertEqual(is_permutation("vaaa", "bddd"), False)
 
   def test3(self):
-    self.assertEqual(unique("hey tom"), True)
+    self.assertEqual(is_permutation("vaaaa", "vaaaaaa"), False)
 
     
 if __name__ == '__main__':
-  unittest.main()
+  print(is_permutation("avaa", "vaaa"))
+  # unittest.main()
